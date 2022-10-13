f8c52af6d26493ca6b399a14e0797ea0ce801910
Ashit <ashitpersistent@gmail.com>
2022-10-13 16:48:37 +0530

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="card">
+
+        <div class="delete">
+          <slot name="delete"/>
+        </div>
+
+        <div class="update">
+          <slot name="update"/>
+        </div>
+
+        <div class="title">
+            <slot name="title"/>
+        </div>
+
+
+        <div class="content">
+            <slot name="content"/>
+        </div>
+
+        <div class="description">
+            <slot name="description"/>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts">
+export default{
+
+}
+</script>
+
+<style scoped >
+
+.card {
+  border: 1px solid silver;
+  border-radius: 8px;
+  max-width: 200px;
+  margin: 0 5px;
+  cursor: pointer;
+  box-shadow: 0px 1px 3px darkgrey;
+  transition: 0.2s;
+}
+.title, .content, .description,.delete {
+  padding: 16px;
+  text-transform: capitalize;
+  text-align: center;
+}
+.title, .content {
+  border-bottom: 1px solid silver;
+}
+.title {
+  font-size: 1.25em;
+}
+.card:hover {
+  transition: 0.2s;
+  box-shadow: 0px 1px 9px darkgrey;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,142 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <form onsubmit="return false" >
+      <button @click="generateCard">Add</button>
+      <input type="text"   v-model="inp">
+    </form>
+
+    <div class="cards">
+      <Card
+      v-for="loc in weatherLocation"
+      :key="loc.id"
+      >
+        <template v-slot:delete>
+          <button @click="deleteEl(loc.id)">delete</button>
+        </template>
+        <template v-slot:update>
+            <button @click="updateEl(loc.id)">edit</button>
+            <input type="text"
+            v-model="updateVal"
+
+            v-if="flags.checkUpdateStatus"
+            >
+            <button
+            v-if="flags.checkUpdateStatus"
+            @click="saved(loc.id)"
+          >update</button>
+        </template>
+        <template v-slot:title>
+          {{loc.location}}
+        </template>
+
+        <template v-slot:content >
+          <img src="https://www.google.com/maps/vt/data=JDzAvDVk4ptHwCeUBZbtFsTvzO9B9YMnvFQJvN1ywS3mD3kqWY7woH8wQKAgj0p56pqBCz7AmiNv5lI6WmOYYccp60MXPTvKp7nS3Eh3avliWd62_-dtOmHYh5t_QY3PeX2xoWJYDN-NPdddYjYeIjLL5heMfR5DplFDYzPOeDFYZr_m_w_O7PtBADg2h4438mslL-yfbn1RHhJwqUsMVjAZO8CDw9YPnkb-NmdrAai5aQD7aRirqHt0eDwfrXF3qVw" alt="">
+        </template>
+
+         <template v-slot:description >
+          {{"sample"}}
+        </template>
+
+      </Card>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Card from '@/components/Card.vue'
+import { reactive,ref} from 'vue'
+export default{
+  components:{Card},
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+
+
+  setup(){
+
+    let weatherLocation = reactive([{id:0,location:""}]);
+    let inp = ref("")
+    let updateVal = ref("")
+    let id= ref(0)
+    weatherLocation.pop()
+
+    const flags = reactive({
+      checkUpdateStatus:false
+    })
+    
+    const generateCard = () =>{
+      let obj=reactive({
+      id:id.value,
+      location:inp.value,
+      });
+      if(inp.value){
+        weatherLocation.push(obj)
+      }
+      id.value+=1;
+    };
+
+
+    const deleteEl = (a:number) =>{
+            // weatherLocation.splice(a,1)
+              let i=0;
+              for(i=0;i<weatherLocation.length;i++){
+                if(a==weatherLocation[i].id){
+                  break;
+                }
+              }
+      
+            weatherLocation.splice(i,1)
+            console.log(a,"found",weatherLocation[a].id)
+            console.log(weatherLocation)
+    }
+
+    const updateEl = (a:number) =>{
+      console.log("updating..",a)
+      flags.checkUpdateStatus = true;
+
+    }    
+
+    const saved = (a:number)=>{
+      flags.checkUpdateStatus = false;
+      console.log("saved")
+      let i=0;
+              for(i=0;i<weatherLocation.length;i++){
+                if(a==weatherLocation[i].id){
+                  break;
+                }
+      }
+      weatherLocation[i].location = updateVal.value
+
+    }
+    return{
+      generateCard,
+      weatherLocation,
+      inp,
+      deleteEl,
+      updateEl,
+      flags,
+      saved,
+      updateVal
+    }
+  }
+}
 </script>
+
+<style scoped>
+
+.opace{
+  opacity: 0.5;
+}
+.cards{
+  display:flex
+}
+.card:hover{
+  opacity: 1.0;
+}
+
+img{
+  width: 100%;
+}
+
+
+
+
+</style>
