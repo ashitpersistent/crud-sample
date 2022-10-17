8f0314696634ab93bba7518ee05b52605e17beff
Ashit <ashitpersistent@gmail.com>
2022-10-17 11:14:34 +0530

    format document to make it pretty

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,38 +1,37 @@
 <template>
-    <div class="card">
+  <div class="card">
 
-        <div class="delete">
-          <slot name="delete"/>
-        </div>
-
-        <div class="update">
-          <slot name="update"/>
-        </div>
-
-        <div class="title">
-            <slot name="title"/>
-        </div>
-
-
-        <div class="content">
-            <slot name="content"/>
-        </div>
-
-        <div class="description">
-            <slot name="description"/>
-        </div>
+    <div class="delete">
+      <slot name="delete" />
     </div>
+
+    <div class="update">
+      <slot name="update" />
+    </div>
+
+    <div class="title">
+      <slot name="title" />
+    </div>
+
+
+    <div class="content">
+      <slot name="content" />
+    </div>
+
+    <div class="description">
+      <slot name="description" />
+    </div>
+  </div>
 </template>
 
 
 <script lang="ts">
-export default{
+export default {
 
 }
 </script>
 
 <style scoped >
-
 .card {
   border: 1px solid silver;
   border-radius: 8px;
@@ -42,20 +41,27 @@ export default{
   box-shadow: 0px 1px 3px darkgrey;
   transition: 0.2s;
 }
-.title, .content, .description,.delete {
+
+.title,
+.content,
+.description,
+.delete {
   padding: 16px;
   text-transform: capitalize;
   text-align: center;
 }
-.title, .content {
+
+.title,
+.content {
   border-bottom: 1px solid silver;
 }
+
 .title {
   font-size: 1.25em;
 }
+
 .card:hover {
   transition: 0.2s;
   box-shadow: 0px 1px 9px darkgrey;
 }
-
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,29 @@
 <template>
   <div class="home">
-    <form onsubmit="return false" >
+    <form onsubmit="return false">
       <button @click="generateCard">Add</button>
-      <input type="text"   v-model="inp">
+      <input type="text" v-model="inp">
     </form>
 
     <div class="cards">
-      <Card
-      v-for="loc in weatherLocation"
-      :key="loc.id"
-      >
+      <Card v-for="loc in weatherLocation" :key="loc.id">
         <template v-slot:delete>
           <button @click="deleteEl(loc.id)">delete</button>
         </template>
         <template v-slot:update>
-            <button @click="updateEl(loc.id)">edit</button>
-            <input type="text"
-            v-model="updateVal"
-
-            v-if="flags.checkUpdateStatus"
-            >
-            <button
-            v-if="flags.checkUpdateStatus"
-            @click="saved(loc.id)"
-          >update</button>
+          <button @click="updateEl(loc.id)">edit</button>
+          <input type="text" v-model="updateVal" v-if="flags.checkUpdateStatus">
+          <button v-if="flags.checkUpdateStatus" @click="saved(loc.id)">update</button>
         </template>
         <template v-slot:title>
           {{loc.location}}
         </template>
 
-        <template v-slot:content >
+        <template v-slot:content>
           <img :src=locJsonVal alt="">
         </template>
 
-         <template v-slot:description >
+        <template v-slot:description>
           {{"description"}}
         </template>
 
@@ -44,85 +34,85 @@
 
 <script lang="ts">
 import Card from '@/components/Card.vue'
-import { reactive,ref} from 'vue'
+import { reactive, ref } from 'vue'
 
-export default{
-  components:{Card},
+export default {
+  components: { Card },
 
 
 
-  setup(){
+  setup() {
 
-    let weatherLocation = reactive([{id:0,location:""}]);
+    let weatherLocation = reactive([{ id: 0, location: "" }]);
     let inp = ref("")
     let updateVal = ref("")
-    let id= ref(0)
+    let id = ref(0)
     let locJsonVal = ref(null) as any
     weatherLocation.pop()
-    
+
     const flags = reactive({
-      checkUpdateStatus:false
+      checkUpdateStatus: false
     })
 
-    async function getApi(){
-      const info = await fetch("http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&origin=*&piprop=original&titles="+inp.value)
+    async function getApi() {
+      const info = await fetch("http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&origin=*&piprop=original&titles=" + inp.value)
       const json = await info.json();
-      const locJson = json  
+      const locJson = json
       let temp = locJson.query.pages
       temp = temp[Object.keys(temp)[0]]
-      locJsonVal.value =  temp.original.source
+      locJsonVal.value = temp.original.source
       console.log(locJsonVal.value)
     }
 
-    const generateCard = () =>{
+    const generateCard = () => {
       getApi()
-      let obj=reactive({
-      id:id.value,
-      location:inp.value,
+      let obj = reactive({
+        id: id.value,
+        location: inp.value
       });
-      if(inp.value){
+      if (inp.value) {
         weatherLocation.push(obj)
       }
-      id.value+=1;
+      id.value += 1;
     };
 
 
-    const deleteEl = (a:number) =>{
-            // weatherLocation.splice(a,1)
-              let i=0;
-              for(i=0;i<weatherLocation.length;i++){
-                if(a==weatherLocation[i].id){
-                  break;
-                }
-              }
-      
-            weatherLocation.splice(i,1)
-            console.log(a,"found",weatherLocation[a].id)
-            console.log(weatherLocation)
+    const deleteEl = (a: number) => {
+      // weatherLocation.splice(a,1)
+      let i = 0;
+      for (i = 0; i < weatherLocation.length; i++) {
+        if (a == weatherLocation[i].id) {
+          break;
+        }
+      }
+
+      weatherLocation.splice(i, 1)
+      console.log(a, "found", weatherLocation[a].id)
+      console.log(weatherLocation)
     }
 
-    const updateEl = (a:number) =>{
-      console.log("updating..",a)
+    const updateEl = (a: number) => {
+      console.log("updating..", a)
       flags.checkUpdateStatus = true;
 
-    } 
-    
-    
+    }
 
 
-    const saved = (a:number)=>{
+
+
+    const saved = (a: number) => {
       flags.checkUpdateStatus = false;
       console.log("saved")
-      let i=0;
-              for(i=0;i<weatherLocation.length;i++){
-                if(a==weatherLocation[i].id){
-                  break;
-                }
+      let i = 0;
+      for (i = 0; i < weatherLocation.length; i++) {
+        if (a == weatherLocation[i].id) {
+          break;
+        }
       }
       weatherLocation[i].location = updateVal.value
 
     }
-    return{
+    return {
       generateCard,
       weatherLocation,
       inp,
@@ -138,22 +128,19 @@ export default{
 </script>
 
 <style scoped>
-
-.opace{
+.opace {
   opacity: 0.5;
 }
-.cards{
-  display:flex
+
+.cards {
+  display: flex
 }
-.card:hover{
+
+.card:hover {
   opacity: 1.0;
 }
 
-img{
+img {
   width: 100%;
 }
-
-
-
-
 </style>
